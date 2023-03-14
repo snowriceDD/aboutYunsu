898dd6975fcddcd9a17fd7addbd72cd87e2fe8b4
snowriceDD <yoons100457@gmail.com>
2023-03-14 20:41:51 +0900

    "feat: nav bar 완성"

--- aboutYunsu-vite/src/components/navbar/Navbar.tsx
+++ aboutYunsu-vite/src/components/navbar/Navbar.tsx
@@ -1,14 +1,41 @@
-import { useNavigate } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import * as S from "./styled";
 
 export const Navbar = () => {
-  const navigate = useNavigate();
+
+  const activeStyle = {
+    color: 'white'
+  }
+  const nonActiveStyle = {
+    color: 'rgba(255, 255, 255, 0.4)'
+  }
 
   return (
-    <div>
-      <S.NavList onClick={() => navigate('/about')} >About</S.NavList>
-      <S.NavList onClick={() => navigate('/works')} >Works</S.NavList>
-      <S.NavList onClick={() => navigate('/experience')} >Experience</S.NavList>
-    </div>
+    <S.Container>
+      <S.NavList>
+        <NavLink
+            to='/about'
+            style={({isActive}) => isActive ? activeStyle : nonActiveStyle}
+          >
+            About
+        </NavLink>
+      </S.NavList>
+      <S.NavList>
+        <NavLink
+            to='/works'
+            style={({isActive}) => isActive ? activeStyle : nonActiveStyle}
+          >
+            Works
+        </NavLink>
+      </S.NavList>
+      <S.NavList>
+        <NavLink
+            to='/experience'
+            style={({isActive}) => isActive ? activeStyle : nonActiveStyle}
+          >
+            Experience
+        </NavLink>
+      </S.NavList>
+    </S.Container>
   );
 };
--- aboutYunsu-vite/src/components/navbar/styled.tsx
+++ aboutYunsu-vite/src/components/navbar/styled.tsx
@@ -1,6 +1,17 @@
 import Style from "styled-components";
+import { NavLink } from "react-router-dom";
+
+export const Container = Style.div`
+    display: flex;
+    width: 100vw;
+    height: 5vh;
+    justify-content: center;
+`
+
 
 export const NavList = Style.li`
-
-
+    list-style: none;
+    font-size: 1.5rem;
+    font-weight: 700;
+    margin: 10px 20px;
 `
