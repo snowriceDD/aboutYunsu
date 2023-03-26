29a6c2119f17b224676db47a8ebad8bcde318a61
snowriceDD <yoons100457@gmail.com>
2023-03-26 18:01:57 +0900

    "style: make to card grid"

--- aboutYunsu-vite/src/components/works/Items.tsx
+++ aboutYunsu-vite/src/components/works/Items.tsx
@@ -3,8 +3,12 @@ import * as S from '@/components/works/styled';
 export const Items = () => {
 
     return (
-        <S.Block>
-        
-        </S.Block>
+        <S.ItemsCard>
+            <div className='cardHeader'> &gt; [ WEB SITE ]</div>
+            <img className='imgPlace'/>
+            <div className='tag'>tag</div>
+            <div className='mainTitle'>Main Title</div>
+            <div className='subTitle'>sub Title</div>
+        </S.ItemsCard>
     )
 };
--- aboutYunsu-vite/src/components/works/styled.tsx
+++ aboutYunsu-vite/src/components/works/styled.tsx
@@ -7,7 +7,7 @@ export const Container = Style.div`
 
 export const ItemContainer = Style.div`
     display: grid;
-    grid-template-columns: 1fr 1fr;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
     justify-content: space-between;
     gap: 10px;
     width: 60vw;
@@ -16,9 +16,37 @@ export const ItemContainer = Style.div`
     margin: 100px auto;
 `
 
-export const Block = Style.div`
+export const ItemsCard = Style.div`
     width: 100%;
     height: 300px;
     border-radius: 10px;
     background: rgba(255, 255, 255, 0.1);
+    display: grid; 
+    grid-template-columns: 1fr 1fr 1fr 1fr 1fr; 
+    grid-template-rows: 0.5fr 5fr 0.5fr repeat(2, 1fr); 
+    gap: 10px 10px; 
+    grid-template-areas: 
+    "cardHeader cardHeader cardHeader cardHeader cardHeader"
+    "imgPlace imgPlace imgPlace imgPlace imgPlace"
+    "tag tag tag tag tag"
+    "mainTitle mainTitle mainTitle mainTitle mainTitle"
+    "subTitle subTitle subTitle subTitle subTitle"; 
+    color: #ffffff;
+
+    .cardHeader {
+        grid-area: cardHeader;
+        color: #8FFF00;
+      }
+    .imgPlace {
+        grid-area: imgPlace;
+    }
+    .tag {
+        grid-area: tag;
+    }
+    .mainTitle {
+        grid-area: mainTitle;
+    }
+    .subTitle {
+        grid-area: subTitle
+    }
 `
