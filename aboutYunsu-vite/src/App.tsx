b45b7c6bb040eaff88921aba922243c4a7373985
snowriceDD <yoons100457@gmail.com>
2023-04-20 19:31:55 +0900

    "feat: add vercel analytics"

--- aboutYunsu-vite/src/App.tsx
+++ aboutYunsu-vite/src/App.tsx
@@ -1,11 +1,13 @@
 import { Router } from "./Router";
 import { GlobalStyle } from './GlobalStyle';
+import { Analytics } from '@vercel/analytics/react';
 
 const App = () => {
   return (
     <>
       <GlobalStyle />
       <Router />
+      <Analytics />
     </>
   );
 };
