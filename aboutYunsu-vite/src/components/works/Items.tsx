c610a5c914ffea9ad7ad285413c9e8aef9a8c7c3
snowriceDD <yoons100457@gmail.com>
2023-04-20 19:05:27 +0900

    "refec: image load"

--- aboutYunsu-vite/src/components/works/Items.tsx
+++ aboutYunsu-vite/src/components/works/Items.tsx
@@ -1,7 +1,6 @@
 import * as S from '@/components/works/styled';
 import github from '@/assets/github.png';
 import inlink from '@/assets/inlink.png';
-import { useEffect, useState } from 'react';
 
 export interface ItemsParams {
     data: {
@@ -16,17 +15,12 @@ export interface ItemsParams {
 }
 
 export const Items = (data:ItemsParams) => {
-    // const [imgSrc, setImgSrc] = useState('');
-    // useEffect(() => {
-    //     setImgSrc(data.data.imgSrc);
-    // }, [])
 
     return (
         <S.ItemsCard>
             <div className='cardHeader'> &gt; [ {data.data.categories} ]</div>
             {(data.data.link !== "string") ? <a className='inlink' href={data.data.link} target='_blank'><img src={inlink} alt='해당 작품의 배포 주소'></img></a>: <a/>}
             {(data.data.github !== "string") ? <a className='github' href={data.data.github} target='_blank'><img src={github} alt='해당 작품의 github 주소'></img></a> : <a/>}
-            {/* <img className='imgPlace' src={`${data.data.imgSrc}`}/> */}
             <img className='imgPlace' src={data.data.imgSrc}/>
             <div className='tag'>
                 {data.data.tagName}
