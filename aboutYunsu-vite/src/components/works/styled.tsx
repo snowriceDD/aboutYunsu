312aef6ec137b6602f4aa1622a8fc3f9ce1759f6
snowriceDD <yoons100457@gmail.com>
2023-04-20 17:02:21 +0900

    "style: works 이미지 추가, 그리드 수정"

--- aboutYunsu-vite/src/components/works/styled.tsx
+++ aboutYunsu-vite/src/components/works/styled.tsx
@@ -19,16 +19,17 @@ export const ItemContainer = Style.div`
 export const ItemsCard = Style.div`
     width: 100%;
     display: grid; 
-    grid-template-columns: auto; 
-    // grid-template-rows: 0.5fr 5fr 0.5fr 0.5fr 1fr; 
-    grid-template-rows: auto; 
-    gap: 7px 10px; 
+    padding: 5px;
+    grid-template-columns: auto 1fr 1fr 15px 15px; 
+    grid-template-rows: auto 150px 1rem auto auto; 
+    gap: 7px 7px; 
     grid-template-areas: 
-    "cardHeader cardHeader cardHeader cardHeader cardHeader"
+    "cardHeader cardHeader cardHeader inlink github"
     "imgPlace imgPlace imgPlace imgPlace imgPlace"
-    "tag tag tag tag tag"
+    "tag null null null null"
     "mainTitle mainTitle mainTitle mainTitle mainTitle"
     "subTitle subTitle subTitle subTitle subTitle"; 
+
     color: #ffffff;
     border: 1px solid rgba(255, 255, 255, 0.1);
     border-radius: 2px;
@@ -36,20 +37,38 @@ export const ItemsCard = Style.div`
     .cardHeader {
         grid-area: cardHeader;
         color: #599e00;
-        margin: 5px 5px;
         letter-spacing: 0.1em;
         font-size: 0.8rem;
         font-weight: 400;
       }
+    .github {
+        grid-area: github;
+        img {
+            width: 13px;
+            height: 13px;
+            opacity: 0.5;
+        }
+    }
+    .inlink {
+        grid-area: inlink;
+        img {
+            width: 13px;
+            height: 13px;
+            opacity: 0.5;
+        }
+    }
     .imgPlace {
         width: 100%;
-        max-width: 360px;
-        height: auto;
+        // max-width: 360px;
+        height: 100%;
+        max-height: 150px;
         grid-area: imgPlace;
         overflow: hidden;
+        position: relative;
+        object-fit: scale-down;
     }
     .tag {
-        // grid-area: tag;
+        grid-area: tag;
         display: inline-block;
         border: 1px solid #599e00;
         box-sizing: border-box;
