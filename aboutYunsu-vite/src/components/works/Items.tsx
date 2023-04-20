d6eb6308769ba8f186938666d6b6ad7f4819abb8
snowriceDD <yoons100457@gmail.com>
2023-04-20 18:43:02 +0900

    "refec: img src"

--- aboutYunsu-vite/src/components/works/Items.tsx
+++ aboutYunsu-vite/src/components/works/Items.tsx
@@ -16,17 +16,17 @@ export interface ItemsParams {
 }
 
 export const Items = (data:ItemsParams) => {
-    const [imgSrc, setImgSrc] = useState('');
-    useEffect(() => {
-        setImgSrc(data.data.imgSrc);
-    }, [])
+    // const [imgSrc, setImgSrc] = useState('');
+    // useEffect(() => {
+    //     setImgSrc(data.data.imgSrc);
+    // }, [])
 
     return (
         <S.ItemsCard>
             <div className='cardHeader'> &gt; [ {data.data.categories} ]</div>
             {(data.data.link !== "string") ? <a className='inlink' href={data.data.link} target='_blank'><img src={inlink} alt='해당 작품의 배포 주소'></img></a>: <a/>}
             {(data.data.github !== "string") ? <a className='github' href={data.data.github} target='_blank'><img src={github} alt='해당 작품의 github 주소'></img></a> : <a/>}
-            <img className='imgPlace' src={imgSrc}/>
+            <img className='imgPlace' src={`${data.data.imgSrc}`}/>
             <div className='tag'>
                 {data.data.tagName}
             </div>
